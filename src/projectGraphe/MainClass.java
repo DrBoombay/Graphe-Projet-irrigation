7f762cac51fa68a60157641fa9fdedc7b4aa7c6a
Mal3kit <Mal3kit@Mal3kit-PC.home>
2017-02-09 23:43:50 +0100

    Test de l'objet Source. Nouveau nom donné à la classe Main.

--- src/projectGraphe/MainClass.java
+++ src/projectGraphe/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Source S1 = new Source ("S1", "Lac", 250.15, 600.02);
-		
+	
 //*************************TEST*******************************************
 		System.out.print("La source "+S1.getIdSource()+" est de type "+S1.getTypeSource()+". Son débit actuelle en sortie est de ");
 		System.out.print(S1.getDebitSource()+" et est alimentée par un débit de "+S1.getwaterIn()+".");
