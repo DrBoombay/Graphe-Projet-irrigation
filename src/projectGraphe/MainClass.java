2434acc7dfdbf1fb32c4e023c0d396eb43f70499
thegamble <thibaut.strecker@gmail.com>
2017-02-20 18:25:19 +0100

    Changements mineurs

--- src/projectGraphe/MainClass.java
+++ src/projectGraphe/MainClass.java
@@ -24,36 +24,36 @@ public class MainClass {
 		
 // Cannalisations Sources / Puits
 		
-		Cannalisation ad = new Cannalisation ("AD", 30.0, 0.00, a, d, true);
-		Cannalisation ae = new Cannalisation ("AE", 15.0, 0.00, a, e, true);
-		Cannalisation ag = new Cannalisation ("AG", 20.0, 0.00, a, g, true);
-		Cannalisation bd = new Cannalisation ("BD", 20.0, 0.00, b, d, true);
-		Cannalisation be = new Cannalisation ("BE", 05.0, 0.00, b, e, true);
-		Cannalisation bf = new Cannalisation ("BF", 15.0, 0.00, b, f, true);
-		Cannalisation cf = new Cannalisation ("CF", 10.0, 0.00, c, f, true);
-		Cannalisation cg = new Cannalisation ("CG", 10.0, 0.00, c, g, true);
+		Cannalisation ad = new Cannalisation ("AD", 30.0, 0.00, a, d);
+		Cannalisation ae = new Cannalisation ("AE", 15.0, 0.00, a, e);
+		Cannalisation ag = new Cannalisation ("AG", 20.0, 0.00, a, g);
+		Cannalisation bd = new Cannalisation ("BD", 20.0, 0.00, b, d);
+		Cannalisation be = new Cannalisation ("BE", 05.0, 0.00, b, e);
+		Cannalisation bf = new Cannalisation ("BF", 15.0, 0.00, b, f);
+		Cannalisation cf = new Cannalisation ("CF", 10.0, 0.00, c, f);
+		Cannalisation cg = new Cannalisation ("CG", 10.0, 0.00, c, g);
 		
-		Cannalisation da = new Cannalisation ("AD", 0.00, 30.00, d, a, true);
-		Cannalisation ea = new Cannalisation ("AE", 0.00, 15.00, a, e, true);
-		Cannalisation ga = new Cannalisation ("AG", 0.00, 20.00, a, g, true);
-		Cannalisation db = new Cannalisation ("BD", 0.00, 20.00, b, d, true);
-		Cannalisation eb = new Cannalisation ("BE", 0.00, 05.0, b, e, true);
-		Cannalisation fb = new Cannalisation ("BF", 0.00, 15.00, b, f, true);
-		Cannalisation fc = new Cannalisation ("CF", 0.00, 10.00, c, f, true);
-		Cannalisation gc = new Cannalisation ("CG", 0.00, 10.00, c, g, true);
+		Cannalisation da = new Cannalisation ("AD", 0.00, 30.00, d, a);
+		Cannalisation ea = new Cannalisation ("AE", 0.00, 15.00, a, e);
+		Cannalisation ga = new Cannalisation ("AG", 0.00, 20.00, a, g);
+		Cannalisation db = new Cannalisation ("BD", 0.00, 20.00, b, d);
+		Cannalisation eb = new Cannalisation ("BE", 0.00, 05.0, b, e);
+		Cannalisation fb = new Cannalisation ("BF", 0.00, 15.00, b, f);
+		Cannalisation fc = new Cannalisation ("CF", 0.00, 10.00, c, f);
+		Cannalisation gc = new Cannalisation ("CG", 0.00, 10.00, c, g);
 		
 // Cannalisations Supersource / Sources
 		
-		Cannalisation sa = new Cannalisation ("SA", 45.0, 0.00, s, a, true);
-		Cannalisation sb = new Cannalisation ("SB", 25.0, 0.00, s, b, true);
-		Cannalisation sc = new Cannalisation ("SC", 70.0, 0.00, s, c, true);
+		Cannalisation sa = new Cannalisation ("SA", 45.0, 0.00, s, a);
+		Cannalisation sb = new Cannalisation ("SB", 25.0, 0.00, s, b);
+		Cannalisation sc = new Cannalisation ("SC", 70.0, 0.00, s, c);
 		
 // Cannalisations Puits / Superpuits
 		
-		Cannalisation dp = new Cannalisation ("DP", 30.0, 0.00, d, p, true);
-		Cannalisation ep = new Cannalisation ("EP", 10.0, 0.00, e, p, true);
-		Cannalisation fp = new Cannalisation ("FP", 20.0, 0.00, f, p, true);
-		Cannalisation gp = new Cannalisation ("GP", 30.0, 0.00, g, p, true);
+		Cannalisation dp = new Cannalisation ("DP", 30.0, 0.00, d, p);
+		Cannalisation ep = new Cannalisation ("EP", 10.0, 0.00, e, p);
+		Cannalisation fp = new Cannalisation ("FP", 20.0, 0.00, f, p);
+		Cannalisation gp = new Cannalisation ("GP", 30.0, 0.00, g, p);
 		
 		
 //*************************LISTES CANNALISATIONS*********************************************************
